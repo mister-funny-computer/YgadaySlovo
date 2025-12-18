5e246d6e11a707909759ea85cc4730321230b51f
Mister funny computer <tironasaurusnik@gmail.com>
2025-12-18 19:46:32 +0300

    Can write letter and append it to letters

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 print("Давай поиграем. Я загадал слово, тебе нужно его отгадать по буквам")
+
+secret_word = "deltarune"
+attempts = 5
+letters = []
+
+while attempts > 0:
+    letter = input("Введите букву: ")
+    print(f"Твоя буква {letter}")
+    letters.append(letter)
+    print(letters)
